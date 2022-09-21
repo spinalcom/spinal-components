2290a9bd5211c4a01677e8e2de8f2bf9d46aa450
Maodk223 <maadhkc223@gmail.com>
2022-09-21 15:35:38 +0200

    add dist

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,186 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <space-selector class="ma-2" :root-element="el" :max-depth="3" :onopen="onopen"></space-selector>
+      <download-button :file-name="'fichier test'" :data="table" class="ma-2"></download-button>
+      <div class="d-flex flex-row">
+        <pie-card :title="'Test'" :pie-chart-data="pie"></pie-card>
+        <bar-card :title="'Test 2'" :labels="barLabels" :datasets="barDatas"></bar-card>
+      </div>
+      <div class="d-flex flex-row">
+        <stats-card
+            :value="25"
+            :unit="'Km'"
+            :title="'parcourus'"
+            :subtitle="'Aujourd\'hui'"
+        ></stats-card>
+        <stats-card
+            :value="25"
+            :unit="'Km'"
+            :title="'parcourus'"
+            :subtitle="'Aujourd\'hui'"
+        ></stats-card>
+        <stats-card
+            :value="25"
+            :unit="'Km'"
+            :title="'parcourus'"
+            :subtitle="'Aujourd\'hui'"
+        ></stats-card>
+        <stats-card
+            :value="25"
+            :unit="'Km'"
+            :title="'parcourus'"
+            :subtitle="'Aujourd\'hui'"
+        ></stats-card>
+      </div>
+      <paginated-table :table-data="tableData"></paginated-table>
+    </v-main>
+  </v-app>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import TimeSelector from "@/components/TimeSelector.vue";
+import PieCard from "@/components/PieCard.vue";
+import BarCard from "@/components/BarCard.vue";
+import StatsCard from "@/components/StatsCard.vue";
+import DownloadButton from "@/components/DownloadButton.vue";
+import PaginatedTable from "@/components/PaginatedTable.vue";
+import SpaceSelector from "@/components/SpaceSelector";
 
-@Component({
+export default {
+  name: 'App',
+
   components: {
-    HelloWorld,
+    SpaceSelector,
+    PaginatedTable,
+    DownloadButton,
+    StatsCard,
+    BarCard,
+    PieCard,
   },
-})
-export default class App extends Vue {}
+
+  data: () => ({
+    el: { name: "Liste", title: "Titre", dynamicId: 0, color: '#f00' },
+    time: { name: "SEMAINE", value: "week" },
+    pie: [
+      { label: "oui", value: 75 },
+      { label: "non", value: 65 },
+      { label: "peut-être", value: 15 },
+      { label: "je ne sais pas", value: 42 },
+      { label: "joker", value: 52 },
+      { label: "plus tard", value: 64 },
+      { label: "je m'en fous", value: 58 },
+      { label: "serieux", value: 60 },
+      { label: "Qui t'as dit ça", value: 69 }
+    ],
+    barLabels: ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'],
+    barDatas: [
+      {
+        label: 'Km parcourus',
+        backgroundColor: '#153284',
+        data: [15, 12, 13, 10, 18, 8, 2]
+      }
+    ],
+    table: []
+  }),
+
+  computed: {
+    tableData() {
+      return this.table;
+    }
+  },
+
+  methods: {
+    async onopen(item) {
+      return [
+        { name: "aiud", color: '#f56', dynamicId: 1 },
+        { name: "bhsb", color: '#f56', dynamicId: 2 },
+        { name: "oisqjx", color: '#f56', dynamicId: 3 },
+        { name: "bhvfu", color: '#f56', dynamicId: 4 },
+        { name: "dscgf", color: '#f56', dynamicId: 5 },
+      ]
+    }
+  },
+
+  mounted() {
+    setTimeout(() => this.table = [
+      {
+        a:0,
+        b:1,
+        c:10,
+        d:11
+      },
+      {
+        a:1,
+        b:0,
+        c:10,
+        d:11
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+      {
+        a:0,
+        b:1,
+        c:11,
+        d:10
+      },
+    ], 6000)
+  }
+}
+
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="scss">
+.pie-card {
+  width: 30%;
+}
+.bar-card {
+  width: 70%;
+}
+.stat-card {
+  width: 25%;
+}
+.table-card {
+  height: 30%;
 }
 </style>
---
+++ src/components/PaginatedTable.vue
@@ -0,0 +1,149 @@
+<template id="table-card">
+  <v-card class="d-flex flex-column table-card pa-2 ma-2 rounded-lg" elevation="5" outlined>
+    <div ref="test">
+      <div ref="selected">
+        <span class="card-header-text pt-8 pl-3">{{ title }}</span>
+      </div>
+      <div ref="desktopTable">
+        <v-data-table
+          :loading="!loaded"
+          loadingText="Chargement des données"
+          :headers="headers"
+          :items="tableData"
+          :page.sync="page"
+          :height="dataHeight"
+          :items-per-page="10"
+          class="elevation-1 table-data"
+          :footer-props="{
+                    prevIcon: 'mdi-menu-left',
+                    nextIcon: 'mdi-menu-right',
+                    itemsPerPageText: '',
+                    showCurrentPage: true,
+                    itemsPerPageAllText: 'Tout',
+                    itemsPerPageOptions: [5,10,15,-1],
+                    pageText: '',
+                  }"
+          @page-count="pageCount = $event"
+        >
+          <v-progress-linear v-show="!loaded" slot="progress" color="accent" class="progress-bar" indeterminate></v-progress-linear>
+          <template v-slot:no-data>
+            Pas de données disponibles
+          </template>
+        </v-data-table>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "PaginatedTable",
+
+  props: {
+    title: {
+      type: String,
+      default: "Table Card"
+    },
+
+    tableData: {
+      type: Array,
+      required: true
+    }
+  },
+
+  data: () =>({
+    pageCount: 0,
+    page: 1,
+  }),
+
+  computed: {
+    loaded() {
+      return this.tableData.length > 0
+    },
+    headers() {
+      return this.loaded ? Object.keys(this.tableData[0]) : []
+    },
+    dataHeight() {
+      const card = document.querySelector('#table-card')
+      const foot = document.querySelector('.v-data-footer')
+      return card ? card.clientHeight - foot.clientHeight : 165;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.progress-bar {
+  margin-left: -16px;
+  width: calc(100% + 32px);
+}
+
+.card-header-text {
+  font-weight: 500;
+  color: #677088;
+  letter-spacing: 2px;
+  font-size: 14px;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+::v-deep .theme--light.v-pagination .v-pagination__item--active {
+  background-color: #eaeef0 !important;
+  color: #59636b;
+  border: 1px solid #d7dee3 !important;
+}
+
+::v-deep .theme--light.v-pagination .v-pagination__item {
+  background-color: transparent !important;
+  color: #59636b;
+  box-shadow: none;
+  border: 1px solid #d7dee3;
+  min-width: 50px;
+  border-radius: 8px ;
+}
+
+::v-deep .theme--light.v-pagination .v-pagination__navigation {
+  background-color: transparent !important;
+  color: #59636b;
+  box-shadow: none;
+  border: 1px solid #d7dee3;
+}
+
+::v-deep .v-input--selection-controls {
+  margin-top: 0;
+}
+
+::v-deep .v-item-group.v-bottom-navigation .v-btn.v-btn--active {
+  color: #f6f6f6;
+  background-color: #546e7a !important;
+}
+
+.table-data {
+  box-shadow: none !important;
+  background-color: white !important;
+}
+::v-deep .theme--light.v-data-table > .v-data-table__wrapper > table > thead > tr:last-child > th {
+  color: #546e7a;
+}
+
+tr {
+  background-color: transparent !important;
+}
+
+::v-deep .v-app-bar.v-app-bar--fixed{
+  position: relative !important;
+}
+
+::v-deep .v-input--selection-controls .v-input__slot>.v-label, .v-input--selection-controls .v-radio>.v-label {
+  font-size: 12px;
+  font-weight: 500;
+  color: #677088;
+}
+</style>
